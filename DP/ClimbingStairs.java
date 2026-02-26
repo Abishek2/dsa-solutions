cfe48287f77820c63a76d05e54f440de54241d75
Abishek2 <pmsaaa824@gmail.com>
2026-02-26 10:34:37 +0100

    Optimised solution for climbing stairs

--- DP/ClimbingStairs.java
+++ DP/ClimbingStairs.java
@@ -1,17 +1,19 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        return helper(n, memo);
-    }
+       if(n<=2){
+        return n;
+       }
 
-    private int helper(int n, int[] memo) {
-        if (n <= 2) return n;
-        
-        // If we already calculated this step, don't do it again!
-        if (memo[n] != 0) return memo[n];
+       int first = 1;
+       int second = 2;
 
-        // Store the result in our memo array before returning
-        memo[n] = helper(n - 1, memo) + helper(n - 2, memo);
-        return memo[n];
+       for(int i=3; i<=n;i++){
+        int current = first + second;
+
+        first = second;
+        second = current;
+       }
+
+       return second;
     }
 }
