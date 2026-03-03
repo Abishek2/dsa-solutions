795452af876349a97479ab055bc2085674e3e943
Abishek2 <pmsaaa824@gmail.com>
2026-03-03 11:26:10 +0100

    Solution for max consucutive ones problem

---
+++ Arrays_Hashing/MaxConsecutivOnes.java
@@ -0,0 +1,22 @@
+class MaxConsecutivOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int ans = 0;
+        int count =0;
+
+        for(int num: nums){
+            
+            if(num == 1){
+                count += 1;
+                ans = Math.max(ans, count);
+                
+            }
+            else{
+               count = 0;
+            }
+        }
+
+        return ans;
+        
+    }
+}
