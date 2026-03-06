62b2232fcddc478c4fdae6da52491a09deea1a76
Abishek2 <pmsaaa824@gmail.com>
2026-03-06 12:57:15 +0100

    Solution for ReversePolishNotation problem

---
+++ Stack/ReversePolishNotation.java
@@ -0,0 +1,28 @@
+
+import java.util.Stack;
+
+class ReversePolishNotation{
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String s : tokens) {
+            if (s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b); // Java division truncates toward zero by default
+            } else {
+                // It's a number, convert String to Integer
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
